95d840c93766cd1971c46930ed99efe0a3356a8c
ezadin2 <149968485+ezadin2@users.noreply.github.com>
2024-04-19 14:47:40 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,51 +1,40 @@
-const express =require("express")
-const mongoose =require("mongoose")
-const cors =require("cors")
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const RegisterModel = require('./models/Register')
+
 const app = express()
-const UserModel = require("./models/Users")
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017/crud")
-app.get("/",(req,res)=>{
-    UserModel.find({})
-    .then(users=>res.json(users))
-    .catch(err=>res.json(err))
+mongoose.connect('mongodb+srv://ezadinbadru55:esu0940715427@cluster0.qortey9.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0');
+
+
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body;
+    RegisterModel.findOne({email: email})
+    .then(user => {
+        if(user) {
+            res.json("Already have an account")
+        } else {
+            RegisterModel.create({name: name, email: email, password: password})
+            .then(result => res.json(result))
+            .catch(err => res.json(err))
+        }
+    }).catch(err => res.json(err))
 })
 
-app.post("/createUser", (req, res) => {
-    UserModel.create(req.body)
-        .then(users => res.json(users))
-        .catch(err => res.json(err));
-});
 
-app.delete("/deleteUser/:id",(req,res)=>{
-    const id = req.params.id;
-    UserModel.findByIdAndDelete(id)
-        .then(() => res.json({ message: 'User deleted successfully' }))
-        .catch(err => res.status(500).json(err));
+app.listen(3001, () => {
+    console.log("Server is Running")
 })
-
-app.put("/updateUser/:id", (req, res) => {
-    const id = req.params.id;
-    UserModel.findByIdAndUpdate(
-      { _id: id },
-      { name: req.body.name, email: req.body.email, age: req.body.age },
-      { new: true } // Return the updated document
-    )
-      .then((user) => res.json(user))
-      .catch((err) => res.json(err));
-  });
-  
-app.get("/getUser/:id",(req,res)=>{
-    const id = req.params.id;
-    UserModel.findById({_id:id})
-    .then(users=>res.json(users))
-    .catch(err=>res.json(err))
-})
-
-app.listen(3001,()=>{
-    console.log("server running");
-})
-
 
